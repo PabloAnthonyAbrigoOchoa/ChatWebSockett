fb8b3f0f938d391b2f98bee00e3e121d974d3f89
Pablo Abrigo Ochoa <paabrigo@tecnologicoloja.edu.ec>
2021-08-17 22:40:08 -0500

    Registro - Usuario

---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require ('express');
+const app = express();
+
+//configuraciones iniciales
+app.set('port', process.env.PORT || 3000);
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+//iniciamos el server
+const server = app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+//declaramos websockets
+io.on('connection',(socket) => {
+    console.log("nueva conección", socket.id);
+    socket.on('chat:message',(data) => {
+        io.sockets.emit('chat:message', data);
+    })
+});
---
+++ public/login-index.js
@@ -0,0 +1,7 @@
+let log_user = document.getElementById('login_username');
+let log_password = document.getElementById('login_password');
+let buton = document.getElementById('login_registro').addEventListener("click", guardar_usuario);
+
+function guardar_usuario(){
+   
+}
